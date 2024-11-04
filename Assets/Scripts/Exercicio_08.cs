5adbf749074fa48ea707976345fe196c0815d420
ARTHUR REIS DE OLIVEIRA <49198028898@info.local>
2024-11-04 17:29:55 -0300

    exercicios 8 a 10

---
+++ Assets/Scripts/Exercicio_08.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+/*
+    8. (Contador de horas e dias) Crie um script que em que uma
+    variável inteira hora seja incrementada de uma unidade a cada 10
+    segundos e volte a ser 0 quando alcançar o valor 24. Quando
+    completar um ciclo, incremente uma variável dias e escreva o
+    número de dias que se passaram no console. (Coloque o código
+    dentro da função-evento Update).
+ */
+
+public class Exercicio_08 : MonoBehaviour
+{
+
+    int hora = 0;
+    int dia = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hora < 24)
+        {  
+            hora++; 
+        }
+        else
+
+    }
+}
---
+++ Assets/Scripts/Exercicio_09.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+/*
+    9. (Placar final) Crie um script que lê o placar de uma partida entre
+    time A e time B. Depois, escreva no console qual dos três
+    resultados possíveis aconteceu: vitória do time A, vitória do time B
+    ou empate. Se o empate teve mais de 3 pontos para cada lado,
+    escreva que foi “um empate emocionante”.
+ */
+
+public class Exercicio_09 : MonoBehaviour
+{
+
+    [SerializeField] int timeA = 100;
+    [SerializeField] int timeB = 100;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (timeA > timeB)
+        {
+            print("vitória do time A");
+        }
+        else if (timeA < timeB)
+        {
+            print("vitória do time B");
+        }
+        else if (timeA == timeB)
+        {
+            print("empate");
+        }
+        else if(timeA + 3 == timeB + 3)
+        {
+            print("um empate emocionante");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
