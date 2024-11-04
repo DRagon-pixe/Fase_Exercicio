7f80c6b0458a3c0d4be76dabcf218e0ea1fe74d1
ARTHUR REIS DE OLIVEIRA <49198028898@info.local>
2024-11-04 16:18:08 -0300

    Exercicio 3

--- Assets/Scripts/Exercicio_03.cs
+++ Assets/Scripts/Exercicio_03.cs
@@ -2,10 +2,44 @@ using UnityEngine;
 
 public class Exercicio_03 : MonoBehaviour
 {
+    /*
+        Operadores Lógicos
+
+        && (E / AAND)
+        || (OU / OR)
+        ! (NÃO / NOT)
+     
+     */
+
+    [SerializeField] bool playerVivo;
+    [SerializeField] bool powerUp;
+    //double vidaPlayer = 100;
+
+    int vidaPlayerInicial = 100;
+    double vidaPlayerAtual = 100;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //if(playerVivo == false && powerUp == true)
+        //{
+        //    vidaPlayer = vidaPlayer * 0.5;
+        //    print("Ainda náo, vida atual " + vidaPlayer);
+        //}
+        //else
+        //{
+        //    print("Game Over");
+        //}
+
+        if(vidaPlayerAtual <= 0 && powerUp == true)
+        {
+            vidaPlayerAtual = vidaPlayerInicial * 0.5;
+            print("Ainda náo, vida atual " + vidaPlayerAtual);
+        }
+        else
+        {
+            print("Game Over");
+        }
     }
 
     // Update is called once per frame
