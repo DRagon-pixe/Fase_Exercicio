f15baf3ad82a91046060fb712091ee5da969c04a
ARTHUR REIS DE OLIVEIRA <49198028898@info.local>
2024-11-12 14:41:14 -0300

    Desafio 2

--- Assets/Scripts/Laço Repetição/Desafio 2.cs
+++ Assets/Scripts/Laço Repetição/Desafio 2.cs
@@ -1,11 +1,31 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 
-public class Desafio2 : MonoBehaviour
+public class Desafio02 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //Faça um algoritmo que receba o dia, o mês e o ano em que uma pessoa nasceu, imprima na tela quantos anos,
+    //meses e dias essa pessoa já viveu.Leve em consideração o ano com 365 dias e o mês com 30 dias.
+
+    [SerializeField] int dia;
+    [SerializeField] int mes;
+    [SerializeField] int ano;
+
     void Start()
     {
-        
+        DateTime dataAtual = DateTime.Now;
+        int diaAtual = dataAtual.Day;
+        int mesAtual = dataAtual.Month;
+        int anoAtual = dataAtual.Year;
+
+        int anosVividos = (anoAtual - ano);
+        int mesesVividos = (mesAtual - mes);
+        int diasVividos = (dia - diaAtual);
+
+
+        print("Você viveu um total de : " + anosVividos + " Anos" + "\n: " + mesesVividos + " meses" + "\n: " + diasVividos + " dias");
+
     }
 
     // Update is called once per frame
